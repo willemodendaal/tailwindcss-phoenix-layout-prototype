715e6912facd61c24f1673084f1a0a93ba212aab
Willem <willem@floatpays.co.za>
2022-02-26 09:01:23 +0200

    Tailwind up and running.

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,8 @@ config :phoenix_tailwindcss_play, PhoenixTailwindcssPlayWeb.Endpoint,
   secret_key_base: "2jDmdQsyM+x35F/g6cng30Udl3ogM6qaI8zVJ1AVLVBnWy1N4jC6aR7zdXyIBIY4",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+  tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
